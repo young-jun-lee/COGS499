1c70a63c0cdd92ebdfb6af72b6f95d7f650b59a3
youngjun <yjl.2000@hotmail.com>
2023-01-25 01:01:14 -0500

    basic scraping done

--- server/src/data/fetchData.ts
+++ server/src/data/fetchData.ts
@@ -1,23 +1,36 @@
-import { PlaywrightCrawler, Dataset } from 'crawlee';
+// You don't need to import RequestQueue anymore
+import { CheerioCrawler, Dataset, enqueueLinks, log } from 'crawlee';
 
-// PlaywrightCrawler crawls the web using a headless
-// browser controlled by the Playwright library.
-const crawler = new PlaywrightCrawler({
-    // Use the requestHandler to process each of the crawled pages.
-    async requestHandler({ request, page, enqueueLinks, log }) {
-        const title = await page.title();
-        log.info(`Title of ${request.loadedUrl} is '${title}'`);
+const crawler = new CheerioCrawler({
+    async requestHandler({ request, response, body, contentType, $, log }) {
 
-        // Save results as JSON to ./storage/datasets/default
-        await Dataset.pushData({ title, url: request.loadedUrl });
+        const data = [];
 
-        // Extract links from the current page
-        // and add them to the crawling queue.
-        await enqueueLinks();
+        $('div.courseblock').each((index, el) => {
+            const requirements = ($(el).find('span.text.detail-requirements.margin--default').text())
+
+            let prerequisites = requirements.match(/Prerequisite [A-Z]{4} [0-9]{3}(?:, [A-Z]{4} [0-9]{3})*(?:\.)/);
+            // check if prerequisites exist and remove the "Prerequisite" part
+            log.info(prerequisites);
+            data.push({
+                code: $(el).find('span.text.col-2.detail-code').text(),
+                title: $(el).find('span.text.col-7.detail-title').text(),
+                units: Number($(el).find('span.text.detail-hours_html').text().split(' ')[1]),
+                description: $(el).find('p.courseblockextra').text(),
+                hours: $(el).find('span.text.detail-learning_hours').text().split(': ')[1],
+                prerequisites: prerequisites,
+                // corequisites: corequisites,
+                // exclusions: exclusions
+            });
+        });
+
+        // // Save the data to dataset.
+        await Dataset.pushData({
+            url: request.url,
+            data,
+        })
     },
-    // Uncomment this option to see the browser window.
-    // headless: false,
 });
 
-// Add first URL to the queue and start the crawl.
-await crawler.run(['https://crawlee.dev']);
+// Start the crawler with the provided URLs
+await crawler.run(['https://www.queensu.ca/academic-calendar/arts-science/course-descriptions/cisc/']);
