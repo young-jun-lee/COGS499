68f37a60c11acf3a2089ecdc437af50fc540859e
youngjun <yjl.2000@hotmail.com>
2023-01-31 01:41:32 -0500

    fixed edge case for prereqs ( _ or _ or _ )

--- server/src/data/fetchData.ts
+++ server/src/data/fetchData.ts
@@ -2,36 +2,65 @@
 import { CheerioCrawler, Dataset, enqueueLinks, log } from 'crawlee';
 
 const codeExtractor = (requirements: string) => {
-    let courseCodes: string[] = [];
-
 
     const courseCodeRegex = /((?:(?:[A-Z]{4}\s\d{3})|(?:\([A-Z]{4}\s\d{3}[^()]*\)|\[[A-Z]{4}\s\d{3}[^[\]]*\]|\{[A-Z]{4}\s\d{3}[^{}]*\})))/g
-
     const match = requirements.match(courseCodeRegex);
 
     if(match !== null){
         const courseCodesString: string = match[0];
-          const splitString = courseCodesString.split(/(\s+|\(|\)|\[|\])/g).filter((e) => e.trim().length > 0);
-          // go through the array and check if there's a four character string, if there is concatenate it with the next element
-          for (let i = 0; i < splitString.length; i++){
-            if (splitString[i].length === 4){
-              splitString[i] = splitString[i] + " " + splitString[i + 1];
-              splitString.splice(i + 1, 1);
-            }
+        const splitString = courseCodesString.split(/(\s+|\(|\)|\[|\])/g).filter((e) => e.trim().length > 0);
+        // go through the array and check if there's a four character string, if there is concatenate it with the next element
+        for (let i = 0; i < splitString.length; i++){
+          if (splitString[i].length === 4){
+            splitString[i] = splitString[i] + " " + splitString[i + 1];
+            splitString.splice(i + 1, 1);
           }
-          // get rid of empty strings
-          splitString.filter((e) => e.trim().length > 0);
+        }
+        // get rid of empty strings
+        splitString.filter((e) => e.trim().length > 0);
 
 
         console.log("\nbefore parsing: ") 
         console.log(splitString);
 
+        // function parseCourses(array) {
+        //   const stack = [];
+        //   let courses = [];
+        //   let i = 0;
+        //   if (array[0] ==="(" && array[array.length - 1] === ")") {
+        //     array.shift();
+        //     array.pop();
+        //   }
+        //   while (i < array.length) {
+        //     if (array[i].match(/\b[A-Z]{4}\s\d{3}\b/)) {
+        //       courses.push(array[i]);
+        //       if (array[i + 1] === 'or') {
+        //         i++;
+        //         continue;
+        //       }
+        //     } else if (array[i] === '[' || array[i] === '(') {
+        //       stack.push(courses);
+        //       courses.push([]);
+        //       courses = courses[courses.length - 1];
+        //     } else if (array[i] === ']' || array[i] === ')') {
+        //       courses = stack.pop();
+        //     }
+        //     i++;
+        //   }
+        //   return courses;
+        // }
+
+        // the above function works for all cases except for the following:
+        // (CISC 101 or CISC 102)
+        // it returns [ 'CISC 101', 'CISC 102'  ] instead of [ [ 'CISC 101', 'CISC 102' ] ]
+        // so we need to check if the first element is a square bracket, if it is, then we need to wrap the array in another array
+        // the function shoul instea look like this:
         function parseCourses(array) {
           const stack = [];
           let courses = [];
           let i = 0;
+
           if (array[0] ==="(" && array[array.length - 1] === ")") {
-            // remove the first and last element
             array.shift();
             array.pop();
           }
@@ -51,8 +80,22 @@ const codeExtractor = (requirements: string) => {
             }
             i++;
           }
+          let numCourses = 0
+          let numOr = 0
+          for (let i = 0; i < array.length; i++){
+            if (array[i].match(/\b[A-Z]{4}\s\d{3}\b/)){
+              numCourses++;
+            }
+            if (array[i] === "or"){
+              numOr++;
+            }
+          }
+          if (numCourses === numOr + 1 && courses.length !== 1){
+            courses = [courses]
+          }
           return courses;
         }
+
           
 
 
@@ -62,9 +105,6 @@ const codeExtractor = (requirements: string) => {
         // // first check if the first element is a square bracket, then get all course codes until the next square bracket
         return courseCodes
         }
-
-        // console.log(courseCodes)
-        // return courseCodes
       } 
 
 
@@ -83,14 +123,16 @@ const crawler = new CheerioCrawler({
             let prerequisites: RegExpMatchArray = requirements.match(/Prerequisite(.*?)\./g);
             // convert prerequisites into a string
             let stringPrerequisites: string = prerequisites != null ? prerequisites[0] : "None";
-            // console.log(stringPrerequisites)
-            // remove the word 'Prerequisite' from the string and trim the whitespace
-            // stringPrerequisites = stringPrerequisites.replace('Prerequisite', '').trim()
-            // console.log(stringPrerequisites)
-            let codeCleaned = codeExtractor(stringPrerequisites);
-            if (codeCleaned === undefined){
-              codeCleaned = ["None"]
-            }
+            
+            let corequisites: RegExpMatchArray = requirements.match(/Corequisite(.*?)\./g);
+            let stringCorequisites: string = corequisites != null ? corequisites[0] : "None";
+
+            let corequisitesCleaned = codeExtractor(stringCorequisites);
+
+            // let codeCleaned = codeExtractor(stringPrerequisites);
+            // if (codeCleaned === undefined){
+              // codeCleaned = ["None"]
+            // }
             
 
             data.push({
@@ -99,7 +141,7 @@ const crawler = new CheerioCrawler({
                 units: Number($(el).find('span.text.detail-hours_html').text().split(' ')[1]),
                 description: $(el).find('p.courseblockextra').text(),
                 hours: $(el).find('span.text.detail-learning_hours').text().split(': ')[1],
-                prerequisites: codeCleaned,
+                // prerequisites: codeCleaned,
                 // corequisites: corequisites,
                 // exclusions: exclusions
             });
