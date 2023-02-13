65ba3b557148ef65df2b85079836dd5d38ded8c6
youngjun <yjl.2000@hotmail.com>
2023-02-13 17:06:31 -0500

    specialization forms component done

--- client/src/components/Specialization.tsx
+++ client/src/components/Specialization.tsx
@@ -1,16 +1,37 @@
-import { FC } from "react";
-import { Box } from '@mantine/core';
+import { Box, Button, Radio } from '@mantine/core';
+import { FC, useState } from "react";
+import HeaderContent from "../content/Header";
 
-interface Specialiation {
+
+
+
+interface Specialization {
 	title: string;
 }
 
-const Specialization: FC<Specialiation> = (props) => {
-	return (
 
-		<Box>
-            Specialization: {props.title}
-		</Box>
+
+const Specialization: FC<Specialization> = (props) => {
+    const [specialization, setSpecialization] = useState("");
+	return (
+        specialization === "" ? (
+            <Box>
+                {HeaderContent.specializations.map((specialization) => (
+                    <Radio
+                        key={specialization}
+                        label={specialization}
+                        value={specialization}
+                        onChange={(e) => setSpecialization(e.currentTarget.value)}
+                    />
+                ))}
+            </Box>
+        ) : (
+            <Box>
+                Specialization: {specialization}
+                <Button onClick={()=> setSpecialization("")}>Reset</Button>
+            </Box>
+        )
+
 	);
 };
 
--- client/src/content/Header.ts
+++ client/src/content/Header.ts
@@ -1,4 +1,18 @@
 export default {
     title: 'Hitchhiker\'s Guide to Computing Degree Plans',
-    subheading: 'Welcome! As you fill in the courses you have already taken, you will be informed what courses you still need to graduate. Happy planning!'
+    subheading: 'Welcome! As you fill in the courses you have already taken, you will be informed what courses you still need to graduate. Happy planning!',
+    specializations: [
+        "Artificial Intelligence",
+        "Biomedical Computation",
+        "Data Analytics",
+        "Fundamental Computation",
+        "Game Development",
+        "Security",
+        "Biomedical Computing",
+        "Cognitive Science",
+        "Computer Science",
+        "Computing and the Creative Arts",
+        "Computing, Mathematics, and Analytics",
+        "Software Design",
+    ]
 };
